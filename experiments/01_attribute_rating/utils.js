14c225fae7c85d2e3ea369580224618b2b1c7757
ting <tinglin0708@gmail.com>
2024-02-25 17:08:28 -0800

    added stimuli files and trial objects

--- experiments/01_attribute_rating/utils.js
+++ experiments/01_attribute_rating/utils.js
@@ -1,1 +1,13 @@
 // shuffle clips, select only one (lazy, non-lazy) per sentence pair, determine trial order
+function shuffle_array(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+};
+
+// select clips
+function select_clips() {
+
+}
